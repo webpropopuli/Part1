57c51a2db689546887dd59d67ca248dffbf6ecb0
webpropopuli <webpropopuli@gmail.com>
2018-05-03 18:14:23 -0400

    Lab 1 initial

---
+++ EdX OOP in Cshrp/Lab1/Lab1/Car.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab1
+{
+    public class Car
+    {
+        // Defining properties
+        public string Color { get; set; }
+        public int Year { get; set; }
+        public int Mileage { get; set; }
+
+        // Creates integer variable called "instances" and assigns value to 0
+        private static int instances = 0;
+
+        // Adding a Constructor 
+        // This constructor instantiates a Car() object while only having the car's color and year information available
+        public Car(string color, int year)
+        {
+            this.Color = color;
+            this.Year = year;
+            // Every time the constructor runs, increment "instances"
+            instances++;
+        }
+
+        //Adding another Constructor
+        // This constructor instantiates a Car() object while only having the car's year and mileage information available
+        public Car(int year, int mileage)
+        {
+            this.Year = year;
+            this.Mileage = mileage;
+            // Every time the constructor runs, increment "instances"
+            instances++;
+        }
+
+        public Car()
+        {
+            // Every time the constructor runs, increment "instances"
+            instances++;
+        }
+
+        // Declare static member
+        public static int CountCars()
+        {
+            return instances;
+        }
+    }
+}
---
+++ EdX OOP in Cshrp/Lab1/Lab1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Car1 = new Car();
+            var Car2 = new Car( "Red", 2008 );  
+
+            Car1.Color = "White";
+            Car1.Year = 2010;
+            Car1.Mileage = 11000;
+
+            // Access static members
+            int carCount = Car.CountCars();
+
+            // Output to the console window
+            Console.WriteLine( $"There are {carCount} cars on inventory right now." );
+        }
+    }
+}
